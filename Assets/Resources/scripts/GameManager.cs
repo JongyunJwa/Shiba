9c9ff5b2d15b6b4aa30db3c8891aa5f510989146
DESKTOP-RAI6PNS\user <bomnaleunganda97@gmail.com>
2017-11-13 14:33:12 +0900

    11/13 물고기에 부딪히면 카운트되며 물고기 사라지는 기능 구현

---
+++ Assets/Resources/scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    public int fishCount = 0;
+    public Text  fishText;
+
+    void CountUpFish()
+    {
+        fishCount++;
+        fishText.text = "고등어: " + fishCount + "마리";
+        Debug.Log("고등어:" + fishCount);
+    } 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
